f0b71f42d797f7f861002f2d0f27032fdf2086a6
ShaghayeghMrdn <shaghayeghmardani@gmail.com>
2019-08-12 01:26:14 -0700

    common_to_proto updates the response body and the needed headers for common html

--- src/frontend/common_to_proto.cc
+++ src/frontend/common_to_proto.cc
@@ -28,24 +28,25 @@ int main( int argc, char *argv[] )
 {
     try {
         if ( argc < 3 ) {
-            throw runtime_error( "Usage: " + string( argv[ 0 ] ) + " modified_file replayshell_file" );
+            throw runtime_error( "Usage: " + string( argv[ 0 ] ) + " common_html.br replayshell_file" );
         }
 
-        string modified_file = argv[ 1 ];
+        string common_html = argv[ 1 ];
         string proto_file = argv[ 2 ];
 
         /* read in modified text file */
-        string str, total;
+        string str, content = "";
         ifstream in;
-        FileDescriptor fd( SystemCall( "open", open( modified_file.c_str(), O_RDONLY ) ) );
-        in.open( modified_file.c_str() );
+        FileDescriptor fd( SystemCall( "open", open( common_html.c_str(), O_RDONLY ) ) );
+        in.open( common_html.c_str() );
         getline( in, str );
         while ( in ) {
-            total += (str + "\n");
+            content += (str + "\n");
             getline( in, str );
         }
         /* remove extra /n at the end */
-        total = total.substr( 0, total.size() - 1 );
+        content = content.substr( 0, content.size() - 1 );
+        cout << "Content-Length = " << content.length() << endl;
 
         MahimahiProtobufs::RequestResponse protobuf;
 
@@ -59,17 +60,41 @@ int main( int argc, char *argv[] )
         }
 
         MahimahiProtobufs::RequestResponse final_protobuf;
-        MahimahiProtobufs::HTTPMessage new_response( protobuf.response() );
+        MahimahiProtobufs::HTTPMessage old_response( protobuf.response() );
+        MahimahiProtobufs::HTTPMessage final_new_response;
 
-        /* use modified text file as new response body */
-        new_response.set_body( total );
+        final_new_response.set_first_line( protobuf.response().first_line() );
+
+        for ( int i = 0; i < old_response.header_size(); i++ ) {
+            HTTPHeader current_header( old_response.header(i) );
+            if ( HTTPMessage::equivalent_strings( current_header.key(), "Content-Encoding" ) ||
+                 HTTPMessage::equivalent_strings( current_header.key(), "Transfer-Encoding" ) ||
+                 HTTPMessage::equivalent_strings( current_header.key(), "Content-Security-Policy" ) ||
+                 HTTPMessage::equivalent_strings( current_header.key(), "Content-Length" ) ) {
+                cerr << "old value:" << current_header.value() << endl;
+                continue;
+            } else {
+                final_new_response.add_header()->CopyFrom( current_header.toprotobuf() );
+            }
+        }
+
+        HTTPHeader encoding_header( "Content-Encoding: br" );
+        final_new_response.add_header()->CopyFrom( encoding_header.toprotobuf() );
+
+        string length = to_string( content.length() );
+        HTTPHeader length_header( "Content-Length: " + length );
+        final_new_response.add_header()->CopyFrom( length_header.toprotobuf() );
+
+        /* use common html as new response body */
+        final_new_response.set_body( content );
+
 
         /* create new request/response pair using old request and new response */
         final_protobuf.set_ip( protobuf.ip() );
         final_protobuf.set_port( protobuf.port() );
         final_protobuf.set_scheme( protobuf.scheme() );
         final_protobuf.mutable_request()->CopyFrom( protobuf.request() );
-        final_protobuf.mutable_response()->CopyFrom( new_response );
+        final_protobuf.mutable_response()->CopyFrom( final_new_response );
 
         /* delete previous version of protobuf file */
         if( remove( proto_file.c_str() ) != 0 ) {
